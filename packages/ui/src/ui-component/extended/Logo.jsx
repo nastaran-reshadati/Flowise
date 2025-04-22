ace21a5ba9fb164509ba4df50f74f8405ebd61f9
nastaran reshadati <96713994+nastaran-reshadati@users.noreply.github.com>
2025-04-22 08:51:31 +0330

    change logo & title

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,5 @@
-import logo from '@/assets/images/flowise_logo.png'
-import logoDark from '@/assets/images/flowise_logo_dark.png'
+import logo from '@/assets/images/logo.png'
+import logoDark from '@/assets/images/logo.png'
 
 import { useSelector } from 'react-redux'
 
@@ -10,11 +10,7 @@ const Logo = () => {
 
     return (
         <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row' }}>
-            <img
-                style={{ objectFit: 'contain', height: 'auto', width: 150 }}
-                src={customization.isDarkMode ? logoDark : logo}
-                alt='Flowise'
-            />
+            <img style={{ objectFit: 'contain', height: 40, width: 40 }} src={customization.isDarkMode ? logoDark : logo} alt='Flowise' />
         </div>
     )
 }
